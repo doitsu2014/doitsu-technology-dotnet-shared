7706012e4d9d91b963d9c6228138b1e4b9bd41c4
TRAN HUU DUC <duc.tran@doitsu.tech>
2022-02-26 15:31:21 +0700

    Update

---
+++ DoitsuTechnology.Shared/src/DoitsuTechnology.Shared.Infrastructure/BaseEntityConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using DoitsuTechnology.Shared.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DoitsuTechnology.Shared.Infrastructure;
+
+public abstract class BaseEntityConfiguration<T> : IEntityTypeConfiguration<T>
+    where T : class
+{
+    public void Configure(EntityTypeBuilder<T> builder)
+    {
+        if (typeof(T).GetInterfaces().Any(x => x == typeof(IHasDomainEvent)))
+        {
+            builder.Ignore(x => ((IHasDomainEvent)x).DomainEvents);
+        }
+    }
+}
